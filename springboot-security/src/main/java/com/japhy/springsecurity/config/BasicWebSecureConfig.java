67a1ab49921779d4f68181be0b182968fb8a8a54
Japhy <920984324@qq.com>
2021-03-16 15:37:38 +0800

    1. add cloud 2. regular update

---
+++ springboot-security/src/main/java/com/japhy/springsecurity/config/BasicWebSecureConfig.java
@@ -0,0 +1,34 @@
+package com.japhy.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * @author Japhy
+ * @since 2021/3/16 15:24
+ */
+@Configuration
+@ActiveProfiles("basic-secure")
+public class BasicWebSecureConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder delegatingPasswordEncoder =
+            PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+            .passwordEncoder(delegatingPasswordEncoder)
+            .withUser("user").password(delegatingPasswordEncoder.encode("user")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/v1/pri/**").authenticated()
+            .antMatchers("api/pub/v1/**").permitAll()
+            .and().httpBasic();
+    }
+}
---
+++ springcloud-bill/src/main/java/com/japhy/bootstrap/cloud/bill/SpringcloudBillApplication.java
@@ -0,0 +1,13 @@
+package com.japhy.bootstrap.cloud.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudBillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudBillApplication.class, args);
+    }
+
+}
