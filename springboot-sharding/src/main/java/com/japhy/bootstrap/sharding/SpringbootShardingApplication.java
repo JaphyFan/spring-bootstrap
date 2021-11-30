d374303143a45a4bd726296a3ff0d28d600ce6ab
Japhy <920984324@qq.com>
2021-11-30 17:59:47 +0800

    Feature 集成sharding-sphere分库分表方案
    通过sharding-sphere框架实现配置分库分表

---
+++ springboot-sharding/src/main/java/com/japhy/bootstrap/sharding/SpringbootShardingApplication.java
@@ -0,0 +1,13 @@
+package com.japhy.bootstrap.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShardingApplication.class, args);
+    }
+
+}
